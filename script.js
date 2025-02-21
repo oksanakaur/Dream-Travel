803be28366f0381dd442d3cb3784d80932f2d765
oksanakaur <160805189+oksanakaur@users.noreply.github.com>
2025-02-21 14:38:55 +0200

    added some javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+nav
